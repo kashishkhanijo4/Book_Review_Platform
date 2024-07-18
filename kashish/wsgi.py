1c18f1e54778620ddbd40a558e5fbd376df6e2ed
kashish <kashishkhanijo28@gmail.com>
2024-07-18 12:46:22 +0530

    api name changed and server database name changed module changed update the urls and manage.py file and also updated app.py

--- kashish/wsgi.py
+++ kashish/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for andisbook project.
+WSGI config for kashish project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'andisbook.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'kashish.settings')
 
 application = get_wsgi_application()
--- reviews/apps.py
+++ reviews/apps.py
@@ -5,8 +5,23 @@
 #     name = 'reviews'
 # reviews/apps.py
 
+# from django.apps import AppConfig
+
+# class ReviewsConfig(AppConfig):
+#     default_auto_field = 'django.db.models.BigAutoField'
+#     name = 'kashish'
+
+# app1/apps.py
 from django.apps import AppConfig
 
-class ReviewsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'reviews'
+class App1Config(AppConfig):
+    name = 'kashish'
+    label = 'kashish_label'
+
+# kashish/apps.py
+from django.apps import AppConfig
+
+class KashishConfig(AppConfig):
+    name = 'kashish'
+    label = 'kashish_label'  # Ensure this is unique
+
